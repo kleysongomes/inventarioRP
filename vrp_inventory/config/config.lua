d4988e75ac6741e953204a5dab56204df515d297
Kleyson Gomes <kleysongomes7@gmail.com>
2021-08-23 22:17:39 -0300

    novo modelo de inventario adicionado

---
+++ vrp_inventory/config/config.lua
@@ -0,0 +1,12 @@
+config = {}
+
+config.webhookDrop = 'https://discord.com/api/webhooks/750897927072776212/Kq84UC8G-KC2BA2yTjIkSM8GqIg9jz4Hn1litD5oblNhohrFjtf8pCL2riWfAzLTbz8s'
+config.webhookSend = 'https://discord.com/api/webhooks/750897618208686130/6sEtOAKax02kJd_HrJFh3oUG_abeWeYiI--WVj0VFGh8e_1S1IGOk5-ly_FGq6vvItYN'
+config.webhookEquip = 'https://discord.com/api/webhooks/750897490223562771/jgsioNI-X7FQ9kY8pUdYfhI0IWSFrx4qaF63pm6lqAjPawpxtBXcy3agYGEH1lBmLeCH'
+config.webhookUnEquip = 'https://discord.com/api/webhooks/750897823133728849/efRzZYWhrIfggAOXfdwqk-qQjD-yvTFHretzIsmMOmdIMoDz_HcRwkbeVyBtlbfMG-J2'
+
+config.webhookColor = 16431885
+config.webhookIcon = 'https://cdn.discordapp.com/attachments/780423585264304128/792390824003633152/Castiel.png'
+config.webhookBottom = 'By:Castiel - '
+
+config.imageServer = 'http://136.243.58.46'
